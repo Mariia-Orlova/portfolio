7a3554539436d6897f4b6586dbac4875e9d7ac21
Victor <v_arab@ua.fm>
2021-03-08 19:48:14 +0200

    suppleanddemandbegin

---
+++ cypress/support/patterns/suppleAndDemand/SuppleDemandElements.js
@@ -0,0 +1,10 @@
+export class SuppleDemandElements {
+
+    menuSuppleDemand= '[class=\'nav-link active accent\']';
+    createButton = '[class=\'document-add ml-16 br-20 mat-flat-button mat-button-base mat-primary ng-trigger ng-trigger-animate\']';
+
+
+
+
+}
+export const suppleDemandElements = new SuppleDemandElements();
