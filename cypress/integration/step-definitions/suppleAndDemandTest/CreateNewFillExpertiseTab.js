c1af1388588fb4b781a4fb6b9145e3602c413881
Tech2bua <tech2bua@gmail.com>
2021-07-07 22:55:30 +0300

    fix create new tests for Supple and Demand

---
+++ cypress/integration/step-definitions/suppleAndDemandTest/CreateNewFillExpertiseTab.js
@@ -0,0 +1,56 @@
+import {When,Then, And} from "cypress-cucumber-preprocessor/steps";
+import {suppleDemandElements} from "../../../support/patterns/suppleAndDemand/SuppleDemandElements";
+
+When ('Fill data for Expertise tab', () => {
+    suppleDemandElements
+        .expertiseLabelText();
+    suppleDemandElements
+        .selectCategoryText();
+    suppleDemandElements
+        .selectCategoryEngineering();
+    suppleDemandElements
+        .selectCategoryTechnologies();
+    suppleDemandElements
+        .selectCategoryElectricalEngineering();
+    suppleDemandElements
+        .selectCategoryTransport();
+    suppleDemandElements
+        .selectCategorySoftware();
+    suppleDemandElements
+        .selectCategoryOthers();
+    suppleDemandElements
+        .selectCategoryArticles();
+    suppleDemandElements
+        .selectCategorySurfaceTreatment();
+
+    //cy.get(suppleDemandElements.material)
+    // .eq(0)
+    //.click({force:true});
+    cy.contains ('Advice')
+        .should ('contain', 'Advice')
+        .click({force:true});
+    cy.contains ('Calculation')
+        .should ('contain' , 'Calculation')
+        .click({force:true});
+    cy.contains ('Hydraulic engineering')
+        .should('contain' , 'Hydraulic engineering')
+        .click({force:true});
+    cy.contains ('Mechanical engineering')
+        .should ('contain', 'Mechanical engineering')
+        .click({force:true});
+    cy.contains ('Pneumatic engineering')
+        .should ('contain', 'Pneumatic engineering')
+        .click({force:true});
+    cy.contains ('Technical parts')
+        .should ('contain' , 'Technical parts')
+        .click({force:true});
+    cy.contains (' Others')
+        .should ('contain', ' Others')
+        .click({force:true});
+    cy.contains (' Construction')
+        .should ('contain', ' Construction')
+        .click({force:true});
+
+
+
+});
---
+++ cypress/integration/step-definitions/suppleAndDemandTest/CreateNewFillPublicationTab.js
@@ -0,0 +1,72 @@
+import {When,Then, And} from "cypress-cucumber-preprocessor/steps";
+import {suppleDemandElements} from "../../../support/patterns/suppleAndDemand/SuppleDemandElements";
+
+When ('Fill data for Publication tab', () => {
+    suppleDemandElements
+        .publicationTab();
+    cy.contains('Choose how you want to share the publication below')
+        .should('contain', 'Choose how you want to share the publication below')
+        .scrollIntoView();
+    cy.contains('Public')
+        .should('contain', 'Public');
+    cy.contains('Tender area')
+        .should('contain', 'Tender area');
+    cy.contains('You can choose the countries that your request will be public to (default is all countries)')
+        .should('contain', 'You can choose the countries that your request will be public to (default is all countries)');
+
+    cy.get('.mat-checkbox-inner-container')
+        .find('input')
+        .should('be.checked');
+
+    suppleDemandElements
+        .selectNetherlandsTab();
+    cy.get(suppleDemandElements.selectNetherlands)
+        .eq(0);
+    cy.get(suppleDemandElements.selectNetherlands)
+        .eq(1)
+        .click({force:true});
+    cy.get(suppleDemandElements.selectNetherlands)
+        .eq(2)
+        .click({force:true});
+    cy.get(suppleDemandElements.selectNetherlands)
+        .eq(3)
+        .click({force:true});
+    cy.get(suppleDemandElements.selectNetherlands)
+        .eq(4)
+        .click({force:true});
+    cy.get(suppleDemandElements.selectNetherlands)
+        .eq(5)
+        .click({force:true});
+    cy.get(suppleDemandElements.selectNetherlands)
+        .eq(6)
+        .click({force:true});
+    cy.get(suppleDemandElements.selectNetherlands)
+        .eq(7)
+        .click({force:true});
+    suppleDemandElements
+        .selectTenderArea();
+
+    //Invite
+
+    /*cy.get(suppleDemandElements.clickOnInviteButton)
+        .scrollIntoView()
+        .should('be.visible')
+        .and ('contain', ' Invite ')
+        .and('not.be.disabled')
+        .click({force:true});
+    cy.get ('[class="w-100-p h-100-p dialog-container"]')
+        .should ('be.visible');
+    //select company
+    cy.get ('[mat-raised-button]')
+        .eq(0)
+        .click({force:true});
+    //click on Ok button
+    cy.get ('[data-cy="select-company-button"]')
+        .should('contain', 'Ok')
+        .click({force:true});
+
+    cy.xpath('//span[contains(text(),\'Create\')]')
+        .should('contain' , 'Create')
+        .click({force:true});
+        */
+});
